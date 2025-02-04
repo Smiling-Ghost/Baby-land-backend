14bf1801aaf0752459a417d3f598e7c74e06a246
Qudus <bozz9617@gmail.com>
2025-02-04 06:55:29 -0500

    rate

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const startServer = async () => {
       console.log(`Server is running on  http://localhost:${PORT}`);
     });
   } catch (err) {
-    console.log("Failed to connect to the database:", err.message);
+    console.error("Failed to connect to the database:", err.message);
     process.exit(1);
   }
 };
